f409090f464e16bec6f5545ef07029893a4253e0
bmxmvl <maxbash974@gmail.com>
2026-05-05 21:19:40 +0300

    Update paid_orders.py

--- dags/paid_orders.py
+++ dags/paid_orders.py
@@ -1,1 +1,25 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator 
+from datetime import datetime 
+from functions import execute_sql_script    
 
+OWNER = "{{ OWNER }}" #обеспечивает уникальность дагов по ученикам для деплоя
+
+with DAG(
+          dag_id=f'paid_orders_dag_{OWNER}', #меняет название дага до _dag
+          start_date = datetime(2026, 5, 5),
+          schedule='0 6 * * *', #меняем расписание запуска дага
+          catchup=False,
+          tags=[OWNER],
+          default_args={
+                    "owner": OWNER
+          }
+) as dag:
+# создаем задачу, в которой вызываем python функцию execute_sql_script и передаем в нее название sql скрипта
+          core_customer = PythonOperator( #меняем название задачи
+                  task_id = 'paid_orders', #меняем название задачи
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql//paid_orders'} #меняем название sql скрипта
+          )
+
+core_customer #меняем название задачи
