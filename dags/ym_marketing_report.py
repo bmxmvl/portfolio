e5bcba568389e9c1e79723316feadee89bc13997
bmxmvl <maxbash974@gmail.com>
2026-05-08 13:29:28 +0300

    Update ym_marketing_report.py

--- dags/ym_marketing_report.py
+++ dags/ym_marketing_report.py
@@ -3,23 +3,23 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime 
 from functions import execute_sql_script    
 
-OWNER = "{{ OWNER }}" #обеспечивает уникальность дагов по ученикам для деплоя
+OWNER = "{{ max_b }}" #обеспечивает уникальность дагов по ученикам для деплоя
 
 with DAG(
-          dag_id=f'paid_orders_dag_{OWNER}', #меняет название дага до _dag
-          start_date = datetime(2026, 5, 5),
-          schedule='0 6 * * *', #меняем расписание запуска дага
+          dag_id=f'ym_marketing_report_dag_{OWNER}', #меняет название дага до _dag
+          start_date = datetime(2026, 5, 7),
+          schedule='0 8 * * *', #меняем расписание запуска дага
           catchup=False,
-          tags=[OWNER],
+          tags=[max_b],
           default_args={
-                    "owner": OWNER
+                    "owner": max_b
           }
 ) as dag:
 # создаем задачу, в которой вызываем python функцию execute_sql_script и передаем в нее название sql скрипта
-          paid_orders = PythonOperator( #меняем название задачи
-                  task_id = 'paid_orders', #меняем название задачи
+          ym_marketing_report = PythonOperator( #меняем название задачи
+                  task_id = 'ym_marketing_report', #меняем название задачи
                   python_callable=execute_sql_script,
-                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/paid_orders'} #меняем название sql скрипта
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/ym_marketing_report'} #меняем название sql скрипта
           )
 
-paid_orders #меняем название задачи
+ym_marketing_report #меняем название задачи
