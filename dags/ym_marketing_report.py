c35b59923fc0b58aa1918d072a3e1e17a8d4b683
bmxmvl <maxbash974@gmail.com>
2026-05-08 13:32:49 +0300

    Update ym_marketing_report.py

--- dags/ym_marketing_report.py
+++ dags/ym_marketing_report.py
@@ -3,16 +3,16 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime 
 from functions import execute_sql_script    
 
-OWNER = "{{ max_b }}" #обеспечивает уникальность дагов по ученикам для деплоя
+OWNER = "{{ OWNER }}" #обеспечивает уникальность дагов по ученикам для деплоя
 
 with DAG(
           dag_id=f'ym_marketing_report_dag_{OWNER}', #меняет название дага до _dag
           start_date = datetime(2026, 5, 7),
           schedule='0 8 * * *', #меняем расписание запуска дага
           catchup=False,
-          tags=[max_b],
+          tags=[OWNER],
           default_args={
-                    "owner": max_b
+                    "owner": OWNER
           }
 ) as dag:
 # создаем задачу, в которой вызываем python функцию execute_sql_script и передаем в нее название sql скрипта
