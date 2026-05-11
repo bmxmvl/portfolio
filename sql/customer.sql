4cd36f1786c8551de3e86b7276586abed9d5d3c8
bmxmvl <maxbash974@gmail.com>
2026-05-11 11:42:17 +0300

    Create customer.sql

---
+++ sql/customer.sql
@@ -0,0 +1,15 @@
+with customer as (
+SELECT 
+md5(concat((raw_data::jsonb ->> 'uid')::varchar, (raw_data::jsonb -> 'registration_details' ->> 'registration_dtm')::varchar)) AS event_id,
+(raw_data::jsonb ->> 'uid')::varchar AS customer_id,
+(raw_data::jsonb -> 'registration_details' ->> 'registration_dtm')::timestamp AS event_dtm,
+'registration' AS event_name,
+insert_timestamp AS meta_timestamp,
+'raw.customer' as meta_process_name
+FROM raw.customer
+where insert_timestamp > coalesce((select max(meta_timestamp) from max_b.cjm_mart where meta_process_name = 'raw.customer'),timestamp '1900-01-01')
+)
+
+insert into max_b.cjm_mart
+select *
+from customer
