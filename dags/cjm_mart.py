a4dce6afb113150ae8cfc52b2a054830504497dc
bmxmvl <maxbash974@gmail.com>
2026-05-11 12:10:59 +0300

    Update cjm_mart.py

--- dags/cjm_mart.py
+++ dags/cjm_mart.py
@@ -6,19 +6,51 @@ from functions import execute_sql_script
 OWNER = "{{ OWNER }}"
 
 with DAG(
-          dag_id=f'ym_marketing_report_dag_{OWNER}',
-          start_date = datetime(2026, 5, 7),
-          schedule='0 5 * * *', 
+          dag_id=f'cjm_mart_dag_{OWNER}',
+          start_date = datetime(2026, 5, 10),
+          schedule='0 6,12 * * *'
           catchup=False,
           tags=[OWNER],
           default_args={
                     "owner": OWNER
           }
 ) as dag:
-          ym_marketing_report = PythonOperator( 
-                  task_id = 'ym_marketing_report', 
+          cjm_customer = PythonOperator( 
+                  task_id = 'cjm_customer', 
                   python_callable=execute_sql_script,
-                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/ym_marketing_report'} 
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_customer'} 
           )
 
-ym_marketing_report 
+          cjm_customer_auth = PythonOperator( 
+                  task_id = 'cjm_customer_auth', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_customer_auth'} 
+          )
+
+          cjm_customer_delete = PythonOperator( 
+                  task_id = 'cjm_customer_delete', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_customer_delete'} 
+          )
+
+          cjm_purchase = PythonOperator( 
+                  task_id = 'cjm_purchase', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_purchase'} 
+          )
+
+          cjm_crm_sms = PythonOperator( 
+                  task_id = 'cjm_crm_sms', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_crm_sms'} 
+          )
+
+          cjm_crm_email = PythonOperator( 
+                  task_id = 'cjm_crm_email', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/cjm_crm_email'} 
+          )
+
+[cjm_customer, cjm_customer_auth, cjm_customer_delete, cjm_purchase, cjm_crm_sms, cjm_crm_email]
+
+
