5c8e72e8ce29aac78e3d016f186a0008b2d6dbae
bmxmvl <maxbash974@gmail.com>
2026-05-17 19:39:49 +0300

    Update and rename crm_mart.py to crm_mart_inc.py

---
+++ dags/crm_mart_inc.py
@@ -0,0 +1,24 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator 
+from datetime import datetime 
+from functions import execute_sql_script    
+
+OWNER = "{{ OWNER }}"
+
+with DAG(
+          dag_id=f'ym_marketing_report_dag_{OWNER}',
+          start_date = datetime(2026, 5, 17),
+          schedule='0 7 * * *', 
+          catchup=False,
+          tags=[OWNER],
+          default_args={
+                    "owner": OWNER
+          }
+) as dag:
+          crm_mart_inc = PythonOperator( 
+                  task_id = 'crm_mart', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/crm_mart_inc'} 
+          )
+
+crm_mart_inc
