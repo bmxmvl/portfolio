0d4a0ac0a6c79393f787b4943385a5947757e4ee
bmxmvl <maxbash974@gmail.com>
2026-05-18 10:52:37 +0300

    Create crm_mart_dag_2.py

---
+++ dags/crm_mart_dag_2.py
@@ -0,0 +1,24 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator 
+from datetime import datetime 
+from functions import execute_sql_script    
+
+OWNER = "{{ OWNER }}"
+
+with DAG(
+          dag_id=f'crm_mart_dag_2_{OWNER}',
+          start_date = datetime(2026, 5, 17),
+          schedule='0 7 * * *', 
+          catchup=False,
+          tags=[OWNER],
+          default_args={
+                    "owner": OWNER
+          }
+) as dag:
+          crm_mart_dag_2 = PythonOperator( 
+                  task_id = 'crm_mart_dag_2', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/crm_mart'} 
+          )
+
+crm_mart_dag_2
