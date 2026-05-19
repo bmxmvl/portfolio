71576111a6b361245cccbf849b211031cb91ce83
bmxmvl <maxbash974@gmail.com>
2026-05-19 21:21:43 +0300

    Create ltv_mart_dag.py

---
+++ dags/ltv_mart_dag.py
@@ -0,0 +1,24 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator 
+from datetime import datetime 
+from functions import execute_sql_script    
+
+OWNER = "{{ OWNER }}"
+
+with DAG(
+          dag_id=f'ltv_mart_dag_{OWNER}',
+          start_date = datetime(2026, 5, 19),
+          schedule='0 7 * * 1', 
+          catchup=False,
+          tags=[OWNER],
+          default_args={
+                    "owner": OWNER
+          }
+) as dag:
+          ltv_mart = PythonOperator( 
+                  task_id = 'ltv_mart', 
+                  python_callable=execute_sql_script,
+                  op_kwargs={'file_path': f'/opt/airflow/dags/{OWNER}/dags/sql/ltv_mart'} 
+          )
+
+ltv_mart 
