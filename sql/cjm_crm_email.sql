82bdb64bb7f622340c170859258520bf8419f3a4
bmxmvl <maxbash974@gmail.com>
2026-05-28 20:58:08 +0300

    Update cjm_crm_email.sql

--- sql/cjm_crm_email.sql
+++ sql/cjm_crm_email.sql
@@ -1,4 +1,4 @@
-with crm_email as (
+insert into max_b.cjm_mart
 select distinct on (c.customer_email, event_dtm)
 md5(concat(c.customer_email, (s.raw_data::jsonb ->> 'dtm')::timestamp)) as event_id,
 customer_id,
@@ -12,8 +12,3 @@ on (s.raw_data::jsonb ->> 'email')::varchar = c.customer_email
 and (s.raw_data::jsonb ->> 'dtm')::timestamp > registration_dtm
 where s.insert_timestamp > coalesce((select max(meta_timestamp) from max_b.cjm_mart where meta_process_name = 'raw.crm_email'), timestamp '1900-01-01')
 order by c.customer_email, event_dtm, registration_dtm desc
-)
-
-insert into max_b.cjm_mart
-select *
-from crm_email
