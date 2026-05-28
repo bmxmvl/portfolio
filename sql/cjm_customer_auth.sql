8c6e30b69a3a9a1c8ce6f5f7cf44c037e59d8232
bmxmvl <maxbash974@gmail.com>
2026-05-28 20:59:18 +0300

    Update cjm_customer_auth.sql

--- sql/cjm_customer_auth.sql
+++ sql/cjm_customer_auth.sql
@@ -1,4 +1,4 @@
-with customer_auth as (
+insert into max_b.cjm_mart
 select
 md5(concat(customer_id, auth_date)) as event_id,
 customer_id,
@@ -8,8 +8,3 @@ auth_date as meta_timestamp,
 'raw.customer_auth' as meta_process_name
 from raw.customer_auth
 where auth_date > coalesce((select max(meta_timestamp) from max_b.cjm_mart where meta_process_name = 'raw.customer_auth'), timestamp '1900-01-01')
-)
-
-insert into max_b.cjm_mart
-select *
-from customer_auth
