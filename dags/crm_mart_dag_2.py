c3f65a2c6d7f80bcd037bc47dca98185b9920ab7
bmxmvl <maxbash974@gmail.com>
2026-06-07 16:47:47 +0300

    Update crm_mart_dag_2.py

--- dags/crm_mart_dag_2.py
+++ dags/crm_mart_dag_2.py
@@ -1,6 +1,6 @@
 from airflow import DAG 
 from airflow.operators.python import PythonOperator 
-from datetime import datetime 
+from datetime import datetime
 from functions import execute_sql_script    
 
 OWNER = "{{ OWNER }}"
